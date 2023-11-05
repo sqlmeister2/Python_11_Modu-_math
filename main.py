4916b128d0e58911772457539f67b20dc22f13a4
Mariusz PC <sqlmeister2@gmail.com>
2023-11-05 19:11:11 +0100

    Pierwszy commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# moduły to w innych jezykach biblioteki
+import random
+
+# import zmiennej/ stałej
+from math import pi
+
+#import w taki sposób że możemy nadac nazwę własną funkcji
+from math import sqrt as pierwiastek
+
+
+
+
+#wywołanie funkcji randint z modułu random
+print(random.randint(1,10))
+
+#lub możemy zaimportować całą funkcję randint
+#from random import randint
+#jak tak zaimportujemy to możemy zapisać to jak poniżej
+#print(randint(1, 10))
+print(pi)
+print(pierwiastek(9))
+
+
+
+
